47b5f37ffa016ff1cfbf9dd841b541841d4b9146
Khakim Aidarkhanov <khakimme@gmail.com>
2024-10-20 14:16:00 +0500

    fix: hide SearchInfo if empty count

--- src/components/SearchInfo.jsx
+++ src/components/SearchInfo.jsx
@@ -1,10 +1,13 @@
 export default function SearchInfo({ query, count }) {
-  return (
-    <p className="search-info">
-      <span>
-        You searched for: <strong className="search-info__query">{query}</strong>
-      </span>
-      {count && <span className="search-info__count">{count} results</span>}
-    </p>
-  );
+  if (count) {
+    return (
+      <p className="search-info">
+        <span>
+          You searched for:{" "}
+          <strong className="search-info__query">{query}</strong>
+        </span>
+        {count && <span className="search-info__count">{count} results</span>}
+      </p>
+    );
+  }
 }
