224465d409fa24284d56d59a0d790d6fbc0fa026
Khakim Aidarkhanov <khakimme@gmail.com>
2024-10-20 12:49:41 +0500

    feat: resylte Movie cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,25 @@ export default function MovieCard({ movie }) {
   const { Title, Year, imdbID, Type, Poster } = movie;
 
   return (
-    <article className="card">
-      <img src={Poster} alt={Title} loading="lazy" className="card__img" />
-      <section className="card__info">
+    <article className="movie-card">
+      <div className="movie-card__img-container">
+        {Poster === "N/A" ? (
+          <img
+            src={"placeholder.png"}
+            className="movie-card__img movie-card--placeholder"
+            alt={Title}
+          />
+        ) : (
+          <img
+            src={Poster}
+            alt={Title}
+            loading="lazy"
+            className="movie-card__img"
+          />
+        )}
+      </div>
+
+      <section className="movie-card__info">
         <dl>
           <dt>Name:</dt>
           <dd>{Title}</dd>
